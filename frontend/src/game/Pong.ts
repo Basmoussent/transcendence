2cb8dcc485e231d3466d9442f09ee44772675020
Ines Bennouna <inbennou@f2r7s17.clusters.42paris.fr>
2025-06-30 12:05:42 +0200

    avant merge

--- frontend/src/game/Pong.ts
+++ frontend/src/game/Pong.ts
@@ -282,3 +282,6 @@ export class Pong {
     // ajouter le bouton pong a l'accueil
     // ajouter le bouton accueil
     // stocker les scores API
+
+    // IA
+      // recup le timestamp et laisser une seconde sans rien faire
