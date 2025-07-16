adc93224a93f40e9a1d82865e0f5ea23d5ae6516
Talan <talanlipanda@gmail.com>
2025-07-16 12:28:05 +0200

    refonte architecture websocket files

--- backend/src/routes/web_socket/ws_matchmaking.ts
+++ backend/src/routes/web_socket/ws_matchmaking.ts
@@ -1,47 +1,7 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
-
-/*
+import { FastifyRequest } from 'fastify';
+import { WebSocket } from 'ws';
 
 const matchmaking = new Array<WebSocket>();
-
-export function ws_matchmaking(socket: any, req: FastifyRequest) {
-
-	// Vérification du token
-	const token = req.headers['x-access-token'] ? req.headers['x-access-token'] : req.cookies['x-access-token']; 
-	
-	if (!token) {
-		console.log('⚠️  Aucun token fourni. Détails de la requête :');
-		console.log('Headers :', JSON.stringify(req.headers, null, 2));
-		console.log('Cookies :', JSON.stringify(req.cookies, null, 2));
-		socket.send(JSON.stringify({
-			type: 'notLog',
-			message: 'the user is not log' }))
-		return ;
-	}
-	matchmaking.push(socket);
-	socket.on('message', (message: any) => {
-		try {
-			const data = JSON.parse(message.toString());
-			
-			switch (data.type) {
-				case 'updateUI':
-					broadcastMatchmaking(data);
-					break;
-				default:
-					console.log(`l'event existe pas`)
-			}
-		}
-		catch (error) {
-			console.error('Invalid message format:', error);
-		}
-	});
-	socket.on('close', () => {
-		const id = matchmaking.findIndex(user => user === socket);
-		matchmaking.splice(id, 1);
-
-		console.log(`user left matchmaking`)
-	});
-}
 
 function broadcastMatchmaking(data: any) {
 
@@ -53,4 +13,42 @@ function broadcastMatchmaking(data: any) {
 	}
 }
 
-*/
+export function handleMatchmaking(socket: WebSocket, req: FastifyRequest) {
+	const token = req.headers['x-access-token'] ? req.headers['x-access-token'] : req.cookies['x-access-token']; 
+		
+	if (!token) {
+		console.log('⚠️  Aucun token fourni. Détails de la requête :');
+		console.log('Headers :', JSON.stringify(req.headers, null, 2));
+		console.log('Cookies :', JSON.stringify(req.cookies, null, 2));
+		socket.send(JSON.stringify({
+			type: 'notLog',
+			message: 'the user is not log' }));
+		return ;
+	}
+
+	matchmaking.push(socket);
+	console.log("a user just connected on /matchmaking");
+	socket.on('message', (message: any) => {
+		try {
+			const data = JSON.parse(message.toString());
+			
+			switch (data.type) {
+				case 'updateUI':
+				broadcastMatchmaking(data);
+				break;
+				default:
+				console.log(`l'event existe pas`)
+			}
+		}
+		catch (error) {
+			console.error('Invalid message format:', error);
+		}
+	});
+	socket.on('close', () => {
+		const id = matchmaking.findIndex(user => user === socket);
+		if (id !== -1) {
+			matchmaking.splice(id, 1);
+		}
+		console.log(`user left matchmaking`)
+	});
+}
