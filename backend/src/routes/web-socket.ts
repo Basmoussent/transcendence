96dc65f94d03aa8f4af04fc215a1545cb8056df1
Clement Canis <clemcloum@MacBook-Air-de-Clement.local>
2025-09-17 14:22:19 +0200

    enlever des comments web-socket.ts

--- backend/src/routes/web-socket.ts
+++ backend/src/routes/web-socket.ts
@@ -15,58 +15,6 @@ async function webSocketRoutes(app: FastifyInstance) {
 		handleRoom(app, socket, req);
 	});
 
-	
-	// app.get('/alive', { websocket: true }, async (connection: any, req: FastifyRequest) => {
-	// 	const token = validateToken(req, app);
-	// 	if (!token) {
-	// 		console.log('❌ No token provided for WebSocket connection on /alive');
-	// 		connection.close();
-	// 		return;
-	// 	}
-	// 	try {
-	// 		const decoded = app.jwt.verify(token) as { user: string; name: string };
-	// 		const userId = decoded.name;
-	// 		await redis.set(`online:${userId}`, '1', { EX: 15 });
-	// 		connection.on('message', async (msg: string) => {
-	// 			if (msg === 'ping') {
-	// 				await redis.set(`online:${userId}`, '1', { EX: 60 });
-	// 			}
-	// 		});
-	// 	}
-	// 	catch (error) {
-	// 		console.log('❌ Invalid token for WebSocket connection on /alive');
-	// 		connection.close();
-	// 		return;
-	// 	}
-
-		
-	// });
-
-	// app.get('/alive/:userId', async (req: FastifyRequest, res: FastifyReply) => {
-	// 	const token = validateToken(req, app);
-	// 	if (!token) {
-	// 		console.log('❌ No token provided for WebSocket connection on /alive');
-	// 		res.status(401).send('Unauthorized');
-	// 		return;
-	// 	}
-	// 	try {
-	// 		const decoded = app.jwt.verify(token) as { user: string; name: string };
-	// 		const userId = req.params.userId;
-
-	// 		const isMember = await redis.sIsMember(`online`, userId);
-	// 		if (isMember) {
-	// 			res.status(200).send('alive');
-	// 		  } else {
-	// 			res.status(200).send('dead');
-	// 		  }
-	// 	}
-	// 	catch (error) {
-	// 		console.log('❌ Invalid token for WebSocket connection on /alive');
-	// 		res.status(401).send('Unauthorized');
-	// 		return;
-	// 	}
-	// });
-
 	app.get('/chat', { websocket: true }, async (socket: WebSocket, req: FastifyRequest) => {
 		await handleChat(app, socket, req);
 	});
